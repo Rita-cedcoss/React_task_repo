42da621c80173f3fd9d43bebac471dc03bfeb53a
Rita-cedcoss <ritapal@cedcoss.com>
2022-10-28 18:06:37 +0530

    user_centric task2 completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Task1 from './User_Centric_Design/Task1';
 import './User_Centric_Design/Component.css'
+import Task2 from './User_Centric_Design/Task2';
 function App() {
   return (
     <>
-    <Task1></Task1>
+    {/* <Task1></Task1> */}
+    <Task2></Task2>
     </>
   );
 }
--- src/User_Centric_Design/Task2.js
+++ src/User_Centric_Design/Task2.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Task2 = () => {
   return (
-    <div>Task2</div>
+    
+    <div id="task2_outer">
+     <div id="task2_outer1">
+        <div id="task_Top">
+            <img alt='' height="80px" width="160px" src="oobalogo.png"></img>
+            <h2>Get a Helping Hand with your Home loan</h2>
+        </div>
+        <div id="middle_page">
+            <div id="mid_left">
+              <img className='imgleft' alt='' height="70%" width="100%" src="oobaleft.png"></img>
+            </div>
+            <div id="mid_right">
+              <div id="login_mid">
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+                <p>Do you have a Signed sale agreement for a property</p>
+                <button id="btn_login">PREQUALIFY ME</button>
+              </div>
+            </div>
+        </div>
+     </div>
+     <div id="bottom">
+      <img height="100%" width="100%" src="task2figmgapage.png" />
+     </div>
+    </div>
   )
 }
 
