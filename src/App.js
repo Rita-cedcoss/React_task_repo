0bedbeb2c672c6949f51d1367dea69a9a9049490
Rita-cedcoss <ritapal@cedcoss.com>
2022-10-28 13:09:12 +0530

    Task1 completed of user centric design

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Task1 from './User_Centric_Design/Task1';
+import './User_Centric_Design/Component.css'
+function App() {
+  return (
+    <>
+    <Task1></Task1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/User_Centric_Design/Task1.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Task1 = () => {
+  return (
+    <div id="outerTask1">
+       <div id="top">
+             <div id="Navbar">
+                <div id="left_nav">
+                  <img alt=" " height="50px" width="315px" src="https://university.adespresso.com/wp-content/themes/ae-brew-child/library/images/logo-university.png"/>
+                </div>
+                <div id="right_nav">
+                  <button id="subGreen">Subscribe</button>
+                  <button id="btn_Login">Login</button>
+                </div>
+             </div>
+             <div id="bottom">
+                <div id="bottom_left">
+                  <h1>Wants to make Facebook Advertising easy again?</h1>
+                  <p>Welcome to AdEspresso University!</p>
+                </div>
+                <div id="bottom_right">
+                   <img alt="" height="100%" width="60%" src="figma2.png"></img>
+                </div>
+
+             </div>
+      </div> 
+      <div id="course">   
+      </div>
+      <div id="foot">
+        <p>All members - regardless of their ad spend level - get access to this group! This means you’ll have a range of marketers from all corners of the online industry at your fingertips.</p>
+      </div>
+        
+    </div>
+  )
+}
+
+export default Task1
