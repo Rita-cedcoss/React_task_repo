783658c69564a9674c8dcc234102edbfc8e6bf62
Rita-cedcoss <ritapal@cedcoss.com>
2022-11-04 18:47:27 +0530

    corona tracker task

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <p>
+    Create a react app with following features
+Show total corona virus cases, total deaths and Total recoveries 
+Display country wise data in tabular format where we can sort whole table according to each column 
+Use object key as column name and value as column data
+create a single click for sorting ascending or descending (Toggle)  
+Sample data is attached
+    </p>
+   </>
+  );
+}
+
+export default App;
