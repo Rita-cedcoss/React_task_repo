0ee3cde08bfaf9fc29d2322dca6b17e61133e2f9
Rita-cedcoss <ritapal@cedcoss.com>
2022-11-04 18:35:12 +0530

    custom hooks task completed

--- src/Custom_hooks/CustomHooks.js
+++ src/Custom_hooks/CustomHooks.js
@@ -3,24 +3,44 @@ import useCustomHook from './useCustomHook';
 
 const CustomHooks = () => {
     const [DataArr,setData]=useState([]);
-    useCustomHook(setData);
+    const[pageCount,setPageCount]=useState(0);
+    useCustomHook("https://jsonplaceholder.typicode.com/posts?_start="+pageCount+"&_limit=5",DataArr,setData);
+   
+    useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+            const {scrollHeight,scrollTop,clientHeight}=document.documentElement;
+            if(scrollTop+clientHeight===scrollHeight)
+            {
+              setTimeout(()=>{
+                  setPageCount((count)=>count+5);                
+              },3000)
+              document.getElementById("load").removeAttribute('class','visibility');
+            }
+     })
+   },[])
+   
+
     return (
     <>
     <div id="outer">
         <div id="gridContainer">
            {
-            DataArr.map((item)=>{
+            DataArr.map((item,i)=>{
                 return(
                     <>
                     <div id="grid">
-                      <div id="Title"><h2>{item.title}</h2></div>
+                      <div id="Title"><h3>{item.id}</h3><h2>{item.title}</h2></div>
                       <div id="Content"><p>{item.body}</p></div>
                     </div>
                     </>
                 )
             })
            }
+           {
+            (DataArr.length===100)? <h4>No New Posts Available.......</h4>:  <div id="load" className="visibility"><img height="100%" width="30%" alt="" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"></img></div>
+           }
         </div>
+        
     </div>
     </>
   )
--- src/Custom_hooks/useCustomHook.js
+++ src/Custom_hooks/useCustomHook.js
@@ -1,34 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
-const useCustomHook = (setData) => {
-    // const[limit,setLimit]=useState(5);
-    const[pageCount,setPageCount]=useState(1);
+const useCustomHook = (url,data,setData) => {
     useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts?_start='+pageCount+'_limit=5')
+    fetch(url)
     .then(res=>res.json())
-    .then(result=>{
-        console.log(result);
-        setData(result);  
+    .then(result=>{  
+        data=data.concat(result);
+        setData([...data]);  
+        document.getElementById("load").setAttribute('class','visibility');
     })
-   },[])
+   },[url])
 
-   const showData=()=>{
-    console.log("helo");
-    setTimeout(()=>{
-        let count=parseInt(pageCount)+parseInt(5);
-        setPageCount(count);
-        console.log(pageCount);
-    },300)
-   }
-   window.addEventListener('scroll',()=>{
-    // console.log("hello i am rita");
-          const {scrollHeight,scrollTop,clientHeight}=document.documentElement;
-          if(scrollTop+clientHeight>=scrollHeight)
-          {
-            console.log("hello i am rita");
-            showData();
-          }
-   })
 }
 
 export default useCustomHook
